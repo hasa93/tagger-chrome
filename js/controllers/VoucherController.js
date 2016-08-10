ebb332de4db032fb8d4969d83d9b706f85871443
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-08-10 08:36:08 +0530

    Implement ui flow for validating vouchers

--- js/controllers/VoucherController.js
+++ js/controllers/VoucherController.js
@@ -1,11 +1,21 @@
 angular.module('TaggerApp')
 .controller('VoucherCtrl', function($scope, $state){
 
+	$scope.showValidation = false;
+
 	$scope.goToCreateVoucher = function(){
 		$state.go('cashier.createvoucher');
 	}
 
-	$scope.goToValidateVoucher = function(){
+	$scope.confirmValidation = function(){
 		$state.go('cashier.validatevoucher');
+	}
+
+	$scope.promptValidation = function(){
+		$scope.showValidation = true;
+	}
+
+	$scope.cancelValidation = function(){
+		$scope.showValidation = false;
 	}
 });
--- js/directives/searchpopup.js
+++ js/directives/searchpopup.js
@@ -1,18 +1,16 @@
 angular.module('TaggerApp')
-.directive('modalPopup', function(VoucherService){
+.directive('searchPopup', function(VoucherService){
 	
 	var linkFn = function(scope, elem, attr){
-		VoucherService.getVoucherInfo(0, function(data){
-			scope.vouchers = data;
-			console.log(scope.vouchers);
-		});
+		
 	}
 
 	return{
 		restrict: 'E',
 		scope: {
 			path: '@',
-			cancel: '&'
+			cancel: '&',
+			confirm: '&'
 		},
 		link: linkFn,
 		template: '<ng-include src="path"></ng-include>'
