691e7db42b84af36bc5f541293973e8707d40f6b
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-01 10:52:54 +0530

    Add form validation to cashier sign up form

--- js/controllers/AdminControllers/CashierMgtController.js
+++ js/controllers/AdminControllers/CashierMgtController.js
@@ -1,6 +1,6 @@
 angular.module('TaggerApp')
-.controller('CashierMgtCtrl', function($scope, $state, UserService){
-	
+.controller('CashierMgtCtrl', function($rootScope, $scope, $state, UserService){
+
 	$scope.searchCashier = false;
 
 	var searchType = "update";
@@ -30,14 +30,53 @@ angular.module('TaggerApp')
 		}
 	}
 
+	var validateCashierData = function(){
+		$scope.validation = {};
+		$rootScope.isValid = true;
+
+		console.log($scope.cashier);
+		console.log($scope.validation);
+
+		if(!$scope.cashier.uname || $scope.cashier.uname === ''){
+			$rootScope.isValid = false;
+			$scope.validation.uname = 'Required Field';
+		}
+
+		if(!$scope.cashier.passwd || $scope.cashier.passwd === ''){
+			$rootScope.isValid = false;
+			$scope.validation.passwd = 'Required Field';
+		}
+		else if($scope.cashier.passwd.length < 8){
+			$rootScope.isValid = false;
+			$scope.validation.passwd = "Password must be at least 8 characters";
+		}
+
+		if(!$scope.cashier.lname || $scope.cashier.lname === ''){
+			$rootScope.isValid = false;
+			$scope.validation.lname = 'Required Field';
+		}
+
+		if(!$scope.cashier.fname || $scope.cashier.fname === ''){
+			$rootScope.isValid = false;
+			$scope.validation.fname = 'Required Field';
+		}
+
+
+		//Additional logic for password and user validation is required
+	}
+
 	$scope.createCashier = function(){
 		console.log($scope.cashier);
 
-		UserService.createStaffMember($scope.cashier).then(function(res){
-			console.log(res);
-		}, function(err){
-			console.log(err);
-		});
+		validateCashierData();
+
+		if($rootScope.isValid){
+			UserService.createStaffMember($scope.cashier).then(function(res){
+				console.log(res);
+			}, function(err){
+				console.log(err);
+			});
+		}
 	}
 
 });
