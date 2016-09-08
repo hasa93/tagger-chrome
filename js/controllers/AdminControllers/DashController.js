63b81cfe5615586ba5d9732f5be3977cdd64b0fd
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-08 08:06:20 +0530

    Integrate sockets.io client

--- js/controllers/AdminControllers/DashController.js
+++ js/controllers/AdminControllers/DashController.js
@@ -1,6 +1,12 @@
 angular.module('TaggerApp')
 .controller('DashCtrl', function($scope, PosService){
 
+	var socket = io.connect('http://localhost:3000');
+
+	socket.on('hello', function(message){
+		console.log(message.text);
+	})
+
 	var getInventoryLevels = function(){
 		PosService.getInventoryLevels().then(function(res){
 			$scope.inventoryData = res;
