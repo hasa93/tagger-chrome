e37ee49a2eeda0eecb9d7a0cc0e5383986e6e1a0
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-10 22:00:07 +0530

    Create a notification directive

---
+++ js/directives/notification.js
@@ -0,0 +1,19 @@
+angular.module('TaggerApp')
+.directive('notification', function(){
+	var linkFn = function(scope, elem, attr){
+		scope.close = function(message){
+			scope.action(message);
+		}
+	}
+
+	return{
+		restrict: 'E',
+		link: linkFn,
+		scope:{
+			type: '@',
+			action: '=',
+			text: '@'
+		},
+		templateUrl: "templates/directives/notification.html"
+	}
+})
