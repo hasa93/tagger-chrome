a226db11b9452a8515ded694de87eab7a13939d8
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-11 14:50:48 +0530

    Add directive validation to cashier signup

--- js/controllers/AdminControllers/CashierMgtController.js
+++ js/controllers/AdminControllers/CashierMgtController.js
@@ -44,11 +44,7 @@ angular.module('TaggerApp')
 		$rootScope.isValid = true;
 		console.log($scope.cashier.name);
 
-		if(!$scope.cashier.name && $scope.cashier.name === ''){
-			$rootScope.isValid = false;
-			$scope.validation.search = "Required";
-			return;
-		}
+		$rootScope.$broadcast('SUBMIT');
 
 		UserService.getStaffByName($scope.cashier.name).then(function(response){
 			if(response.length == 0 ) return;
@@ -66,47 +62,16 @@ angular.module('TaggerApp')
 
 	}
 
-	var validateCashierData = function(){
-		$scope.validation = {};
-		$rootScope.isValid = true;
-
-		console.log($scope.cashier);
-		console.log($scope.validation);
-
-		if(!$scope.cashier.uname || $scope.cashier.uname === ''){
-			$rootScope.isValid = false;
-			$scope.validation.uname = 'Required Field';
-		}
-
-		if(!$scope.cashier.passwd || $scope.cashier.passwd === ''){
-			$rootScope.isValid = false;
-			$scope.validation.passwd = 'Required Field';
-		}
-		else if($scope.cashier.passwd.length < 8){
-			$rootScope.isValid = false;
-			$scope.validation.passwd = "Password must be at least 8 characters";
-		}
-
-		if(!$scope.cashier.lname || $scope.cashier.lname === ''){
-			$rootScope.isValid = false;
-			$scope.validation.lname = 'Required Field';
-		}
-
-		if(!$scope.cashier.fname || $scope.cashier.fname === ''){
-			$rootScope.isValid = false;
-			$scope.validation.fname = 'Required Field';
-		}
-
-
-		//Additional logic for password and user validation is required
-	}
-
 	$scope.createCashier = function(){
 		console.log($scope.cashier);
 
+		$rootScope.isValid = true;
+
 		//validateCashierData();
 		$rootScope.$broadcast('SUBMIT', {});
-		
+
+		console.log($rootScope.isValid);
+
 		if($rootScope.isValid){
 			UserService.createStaffMember($scope.cashier).then(function(res){
 				$scope.cashier = { type: 'csh' };
