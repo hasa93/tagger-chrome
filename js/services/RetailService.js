f295817d8ca0947cd5d7875fdd6e150169443c61
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-09-13 22:13:58 +0530

    Add invoice and voucher handling functions

--- js/services/RetailService.js
+++ js/services/RetailService.js
@@ -2,6 +2,29 @@ angular.module('TaggerApp')
 .factory('RetailService', function($q, $http, config){
 	var o = {}
 	var baseApiUrl = config.locals.apiUrl;
+	var invoiceItems = [];
+	var total = 0;
+	var voucherAmount = 0;
+
+	var inInvoice = function(product){
+		for(var i = 0; i < invoiceItems.length; i++){
+			var id = product.id;
+
+			if(id === invoiceItems[i].id){
+				invoiceItems[i].qty += 1;
+				total += product.price;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	var insertToInvoiceList = function(product){
+		if(!inInvoice(product) && product !== undefined){
+				invoiceItems.push(product);
+				total += product.price;
+		}
+	}
 
 	o.createVoucher = function(voucher){
 		var deferred = $q.defer();
@@ -48,6 +71,52 @@ angular.module('TaggerApp')
 		});
 
 		return deferred.promise;
+	}
+
+	o.claimVoucher = function(voucher){
+		voucherAmount += voucher.amount;
+		total -= voucher.amount;
+
+		if(total < 0){
+			total = 0;
+		}
+
+	}
+
+	o.insertProduct = function(product){
+		insertToInvoiceList(product);
+		console.log(invoiceItems);
+	}
+
+	o.removeProduct = function(index){
+		invoiceItems.splice(index, 1);
+	}
+
+	o.dropQuantity = function(index){
+		if(invoiceItems[index].qty == 1){
+			o.removeProduct(index);
+			return;
+		}
+
+		invoiceItems[index].qty -=1;
+	}
+
+	o.getInvoiceList = function(){
+		return invoiceItems;
+	}
+
+	o.getInvoiceTotal = function(){
+		return total;
+	}
+
+	o.getVoucherAmount = function(){
+		return voucherAmount;
+	}
+
+	o.resetInvoice = function(){
+		invoiceItems = [];
+		total = 0;
+		voucherAmount = 0;
 	}
 
 	return o;
