34d853d5a54f79272aa5a639138f3c8c8469e091
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-10-27 22:05:53 +0530

    Initial implementation of statistics view

--- js/controllers/AdminControllers/StatController.js
+++ js/controllers/AdminControllers/StatController.js
@@ -12,19 +12,42 @@ angular.module('TaggerApp')
 		{ name: "Orion" }
 	];
 
-	$scope.prodData = [
-		{ label: 'all', value: 2000 },
-		{ label: 'all', value: 567 },
-		{ label: 'all', value: 1453 },
-		{ label: 'all', value: 400 },
-		{ label: 'all', value: 1998 },
-		{ label: 'all', value: 3452 },
-		{ label: 'all', value: 1456 },
-		{ label: 'all', value: 6434 },
-		{ label: 'all', value: 786 },
-		{ label: 'all', value: 2356 },
-		{ label: 'all', value: 700 }
-	];
+	$scope.prodData = [];
+	$scope.average = 0;
+	$scope.peak = 0;
+	$scope.sum = 0;
+	$scope.items = 0;
+
+	//Generates random data. Should remove from the final
+	var generateDummyData = function(){
+		$scope.prodData = [];
+
+		for(var i = 0; i < 8; i++){
+			$scope.prodData.push({ label: 'all', value: Math.floor(Math.random() * 5000 + 1)});
+		}
+
+		calculateAverage(5);
+		getPeak();
+	}
+
+	var calculateAverage = function(unitPrice){
+		$scope.items = $scope.prodData.length;
+		$scope.sum = $scope.prodData.reduce(function(a, b){
+			return a + b.value;
+		}, 0);
+
+		$scope.sum = $scope.sum * unitPrice;
+		$scope.average = $scope.sum / Math.max($scope.items, 1);
+		console.log("Avg: " + $scope.average);
+		console.log("Sum: " + $scope.sum);
+	}
+
+	var getPeak = function(){
+		$scope.peak = $scope.prodData.reduce(function(a, b){
+			return Math.max(a, b.value);
+		}, 0);
+		console.log("Peak: " + $scope.peak);
+	}
 
 	$scope.updateData = function(prodId){
 		if(prodId !== undefined){
@@ -33,9 +56,12 @@ angular.module('TaggerApp')
 
 		if($scope.prodId === 'all'){
 			StatService.getAllSalesStat($scope.startDate, $scope.endDate).then(function(response){
-				console.log($scope.startDate);
-				console.log($scope.endDate);
 				console.log(response);
+				if(response.length  <= 1){
+					//$scope.prodData = [];
+					generateDummyData();
+					return;
+				}
 
 				$scope.prodData = [];
 
@@ -43,6 +69,8 @@ angular.module('TaggerApp')
 					$scope.prodData.push({ label: item.date, value: item.qty });
 				});
 
+				calculateAverage(5);
+				getPeak();
 				console.log($scope.prodData);
 			});
 		}
@@ -51,17 +79,6 @@ angular.module('TaggerApp')
 				console.log(response);
 			});
 		}
-
-		$scope.max = $scope.prodData.reduce(function(a, b){
-			if(a.qty < b.qty){
-				return b;
-			}
-			else{
-				return a;
-			}
-		});
-
-		console.log($scope.max);
 	}
 
 	PosService.getProductList().then(function(res){
