224f88a70d47a889bcd3190bef97980f8e539a97
Hasanga Somaratne <hasanga.charaka@gmail.com>
2017-01-14 09:02:52 +0530

    More notification style updates

--- js/controllers/AdminControllers/DashController.js
+++ js/controllers/AdminControllers/DashController.js
@@ -1,11 +1,16 @@
 angular.module('TaggerApp')
-.controller('DashCtrl', function($scope, PosService, MessengerService, config){
+.controller('DashCtrl', function($scope, PosService, MessengerService, StatService, config){
 
 	MessengerService.receiveMessages(function(msg){
 		console.log(msg);
 	});
 
 	MessengerService.requestUpdate();
+
+	$scope.startDate = new Date();
+	$scope.endDate = new Date();
+
+	$scope.startDate.setDate($scope.endDate.getDate() - 7);
 
 	var getInventoryLevels = function(){
 		PosService.getInventoryLevels().then(function(res){
@@ -16,7 +21,19 @@ angular.module('TaggerApp')
 	}
 
 	var getSalesData = function(){
-		$scope.salesData = PosService.getSalesData();
+		StatService.getAllSalesStat($scope.startDate, $scope.endDate).then(function(response){
+			console.log(response);
+			if(response.length  <= 1){
+				$scope.prodData = [];
+				return;
+			}
+			$scope.prodData = [];
+
+			response.map(function(item){
+				$scope.prodData.push({ label: item.date.split('T')[0], value: item.qty });
+			});
+			console.log($scope.prodData);
+		});
 	}
 
 	getInventoryLevels();
